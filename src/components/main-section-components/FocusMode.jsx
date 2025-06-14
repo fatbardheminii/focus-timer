8382df4044b703a112e1cd3a67a603072359a3c8
fatbardheminii <fatbardh.emini@hotmail.com>
2025-06-14 19:41:49 +0200

    style: add active class styling to FocusMode buttons
    -Apply active class based on currentMode

--- src/components/main-section-components/FocusMode.jsx
+++ src/components/main-section-components/FocusMode.jsx
@@ -2,23 +2,25 @@ import { useContext } from "react";
 import { TimerContext } from "../../context/TimerContext";
 
 export default function FocusMode() {
-  const { dispatch } = useContext(TimerContext);
+  const { state, dispatch } = useContext(TimerContext);
+  const { currentMode } = state;
+
   return (
     <div className="focus-mode">
       <button
-        className="focus-btn"
+        className={`focus-btn ${currentMode === "focus" ? "active" : ""}`}
         onClick={() => dispatch({ type: "SET_MODE", payload: "focus" })}
       >
         Focus
       </button>
       <button
-        className="short-brake-btn"
+        className={`short-brake-btn ${currentMode === "short" ? "active" : ""}`}
         onClick={() => dispatch({ type: "SET_MODE", payload: "short" })}
       >
         Short Break
       </button>
       <button
-        className="long-brake-btn"
+        className={`long-brake-btn ${currentMode === "long" ? "active" : ""}`}
         onClick={() => dispatch({ type: "SET_MODE", payload: "long" })}
       >
         Long Break
